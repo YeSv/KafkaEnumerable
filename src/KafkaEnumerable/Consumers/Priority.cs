4e3cc196fac930440fe65a2dac22ef98f142230d
YeSv <yegor.sergeev@gmail.com>
2021-12-25 15:40:25 +0200

    Simply overall approach, add new tests

--- src/KafkaEnumerable/Consumers/Priority.cs
+++ src/KafkaEnumerable/Consumers/Priority.cs
@@ -5,143 +5,117 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 
-namespace KafkaEnumerable.Consumers.Priority
+namespace KafkaEnumerable.Consumers.Priority;
+
+public readonly struct PriorityMessage<TKey, TValue>
 {
-    internal readonly struct ConsumeOptions
-    {
-        public readonly bool ReturnNulls;
-        public readonly TimeSpan ConsumeTimeout;
-        public readonly TimeSpan FlushInterval;
-        public readonly int[] Thresholds;
+    readonly object? _data;
 
-        public ConsumeOptions(bool? returnNulls, TimeSpan? consumeTimeout, TimeSpan? flushInterval, int[]? thresholds, int consumersNum)
+    public PriorityMessage(
+        IConsumer<TKey, TValue> consumer,
+        ConsumeResult<TKey, TValue>? result,
+        bool isFlush,
+        int priority) => (Consumer, _data, IsFlush, Priority) = (consumer, result, isFlush, priority);
+
+    public PriorityMessage(
+        IConsumer<TKey, TValue> consumer,
+        ConsumeException error,
+        bool isFlush,
+        int priority) => (Consumer, _data, IsFlush, Priority) = (consumer, error, isFlush, priority);
+
+    public int Priority { get; }
+    public bool IsFlush { get; }
+    public IConsumer<TKey, TValue> Consumer { get; }
+
+    public bool HasData => _data is not null;
+    public bool IsError => _data is not null and ConsumeException;
+
+    public ConsumeException? Error => _data as ConsumeException;
+    public ConsumeResult<TKey, TValue>? ConsumeResult => _data as ConsumeResult<TKey, TValue>;
+}
+
+internal readonly struct ConsumeOptions
+{
+    public readonly bool ReturnNulls;
+    public readonly TimeSpan ConsumeTimeout;
+    public readonly TimeSpan FlushInterval;
+    public readonly int[] Thresholds;
+
+    public ConsumeOptions(bool? returnNulls, TimeSpan? consumeTimeout, TimeSpan? flushInterval, int[]? thresholds, int consumersNum)
+    {
+        ReturnNulls = returnNulls ?? Defaults.ReturnNulls;
+        ConsumeTimeout = consumeTimeout ?? Defaults.ConsumeTimeout;
+        FlushInterval = flushInterval ?? Defaults.FlushInterval;
+        Thresholds = thresholds ?? Enumerable.Repeat(Defaults.Infinity, 1).Concat(Enumerable.Repeat(Defaults.MessagesThreshold, consumersNum - 1)).ToArray();
+    }
+}
+
+internal static class Consumer
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static IEnumerable<PriorityMessage<TKey, TValue>> Consume<TKey, TValue>(IConsumer<TKey, TValue>[] consumers, ConsumeOptions options, CancellationToken token) =>
+        EnumerableOwner.Wrap(Generator(consumers, options, token));
+
+    static IEnumerable<PriorityMessage<TKey, TValue>> Generator<TKey, TValue>(IConsumer<TKey, TValue>[] consumers, ConsumeOptions options, CancellationToken token)
+    {
+        var (priority, checkPriority, nextFlush, currentThresholds) = (0, false, DateTime.UtcNow.Add(options.FlushInterval), options.Thresholds.ToArray());
+        while (true)
         {
-            ReturnNulls = returnNulls ?? Defaults.ReturnNulls;
-            ConsumeTimeout = consumeTimeout ?? Defaults.ConsumeTimeout;
-            FlushInterval = flushInterval ?? Defaults.FlushInterval;
-            Thresholds = thresholds ?? Enumerable.Repeat(Defaults.Infinity, 1).Concat(Enumerable.Repeat(Defaults.MessagesThreshold, consumersNum - 1)).ToArray();
+            if (DateTime.UtcNow > nextFlush) goto Flush; // We need to flush due to timeout
+            if (checkPriority) goto UpdatePriority; // If we need to check priorities - jump
+
+            var message = Consume(consumers[priority], options.ConsumeTimeout, false, priority, token);
+            if (currentThresholds[priority] != Defaults.Infinity) currentThresholds[priority]--; // Update current threshold
+            if (currentThresholds[priority] == 0 || !message.HasData) // Current priority threshold is reached or no meaningful data - reset threshold and try to move to next priority
+            {
+                checkPriority = true;
+                currentThresholds[priority] = options.Thresholds[priority];
+            }
+            if (options.ReturnNulls || message.HasData) yield return message; // Yield message only if it has meaningful data or returnNulls is enabled
+            continue;
+
+
+        UpdatePriority:
+            checkPriority = false;
+            if (++priority >= consumers.Length) priority = 0;
+            for (var i = 0; i < priority; i++) // Check priorities before chosen one
+            {
+                var prevMessage = Consume(consumers[i], options.ConsumeTimeout, false, i, token);
+                if (prevMessage.HasData) // Just check here if previous priority has data (returnNulls is ignored here)
+                {
+                    priority = i; // We can jump to previous priority because consumer returned meaningful data
+                    yield return prevMessage; 
+                    break;
+                }
+            }
+            continue;
+
+        Flush:
+            nextFlush = DateTime.UtcNow.Add(options.FlushInterval); // Update flush deadline
+            for (var i = consumers.Length - 1; i >= 0; i--) // Consume in reverse order to handle repartitioning/(un)assign events
+            {
+                var flushMessage = Consume(consumers[i], options.ConsumeTimeout, true, i, token);
+                if (flushMessage.HasData) yield return flushMessage; // Yield message only if it has meaningful data
+            }
         }
     }
 
-    internal sealed class ConsumerState
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    static PriorityMessage<TKey, TValue> Consume<TKey, TValue>(
+        IConsumer<TKey, TValue> consumer,
+        TimeSpan timeout,
+        bool isFlush,
+        int priority,
+        CancellationToken token)
     {
-        DateTime _nextFlush;
-
-        public int Priority { get; set; }
-        public ConsumeOptions Options { get; set; }
-        public CancellationToken Token { get; set; }
-        public int[] CurrentThresholds { get; set; }
-        public bool ShouldCheckPriorities { get; set; }
-
-        public ConsumerState(ConsumeOptions options, CancellationToken token)
+        token.ThrowIfCancellationRequested();
+        try
         {
-            _nextFlush = DateTime.UtcNow.Add(options.FlushInterval);
-
-            Token = token;
-            Options = options;
-            CurrentThresholds = options.Thresholds.ToArray();
+            return new(consumer, consumer.Consume(timeout), isFlush, priority);
         }
-
-        public bool ShouldFlush()
+        catch (ConsumeException ex)
         {
-            if (_nextFlush > DateTime.UtcNow) return false;
-
-            _nextFlush = DateTime.UtcNow.Add(Options.FlushInterval);
-            return true;
-        }
-    }
-
-    public readonly struct PriorityMessage<TKey, TValue>
-    {
-        readonly object? _data;
-
-        public PriorityMessage(
-            IConsumer<TKey, TValue> consumer, 
-            ConsumeResult<TKey, TValue>? result, 
-            bool isFlush,
-            int priority) => (Consumer, _data, IsFlush, Priority) = (consumer, result, isFlush, priority);
-
-        public PriorityMessage(
-            IConsumer<TKey, TValue> consumer, 
-            ConsumeException error, 
-            bool isFlush,
-            int priority) => (Consumer, _data, IsFlush, Priority) = (consumer, error, isFlush, priority);
-
-        public int Priority { get; }
-        public bool IsFlush { get; }
-        public IConsumer<TKey, TValue> Consumer { get; }
-
-        public bool HasData => _data is not null;
-        public bool IsError => _data is not null and ConsumeException;
-
-        public ConsumeException? Error => _data as ConsumeException;
-        public ConsumeResult<TKey, TValue>? ConsumeResult => _data as ConsumeResult<TKey, TValue>;
-    }
-
-    internal static class Consumer
-    {
-        public static IEnumerable<PriorityMessage<TKey, TValue>> Consume<TKey, TValue>(IConsumer<TKey, TValue>[] consumers, ConsumeOptions options, CancellationToken token) =>
-            Generator(consumers, new(options, token));
-
-        static IEnumerable<PriorityMessage<TKey, TValue>> Generator<TKey, TValue>(IConsumer<TKey, TValue>[] consumers, ConsumerState state)
-        {
-            while (true)
-            {
-                if (state.ShouldFlush()) goto Flush; // We need to flush due to timeout
-                if (state.ShouldCheckPriorities) goto UpdatePriority; // If we need to check priorities - jump
-
-                var message = Consume(consumers[state.Priority], state.Options.ConsumeTimeout, false, state.Priority, state.Token);
-                if (state.CurrentThresholds[state.Priority] != Defaults.Infinity) state.CurrentThresholds[state.Priority]--; // Update current threshold
-                if (state.CurrentThresholds[state.Priority] == 0 || !message.HasData) // Current priority threshold is reached or no meaningful data - reset threshold and try to move to next priority
-                {
-                    state.ShouldCheckPriorities = true;
-                    state.CurrentThresholds[state.Priority] = state.Options.Thresholds[state.Priority];
-                }
-                if (state.Options.ReturnNulls || message.HasData) yield return message; // Yield message only if it has meaningful data or returnNulls is enabled
-                continue;
-
-
-            UpdatePriority:
-                state.ShouldCheckPriorities = false;
-                if (++state.Priority >= consumers.Length) state.Priority = 0;
-                for (var i = 0; i < state.Priority; i++) // Check priorities before current
-                {
-                    var prevMessage = Consume(consumers[i], state.Options.ConsumeTimeout, false, i, state.Token);
-                    if (prevMessage.HasData) // Just check here if previous priority has data (returnNulls is ignored here)
-                    {
-                        state.Priority = i; // We can jump to previous priority because consumer returned meaningful data
-                        yield return prevMessage; 
-                        break;
-                    }
-                }
-                continue;
-
-            Flush:
-                for (var i = consumers.Length - 1; i >= 0; i--) // Consume in reverse order to handle repartitioning/(un)assign events
-                {
-                    var flushMessage = Consume(consumers[i], state.Options.ConsumeTimeout, true, i, state.Token);
-                    if (flushMessage.HasData) yield return flushMessage; // Yield message only if it has meaningful data
-                }
-            }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static PriorityMessage<TKey, TValue> Consume<TKey, TValue>(
-            IConsumer<TKey, TValue> consumer,
-            TimeSpan timeout,
-            bool isFlush,
-            int priority,
-            CancellationToken token)
-        {
-            token.ThrowIfCancellationRequested();
-            try
-            {
-                return new(consumer, consumer.Consume(timeout), isFlush, priority);
-            }
-            catch (ConsumeException ex)
-            {
-                return new(consumer, ex, isFlush, priority);
-            }
+            return new(consumer, ex, isFlush, priority);
         }
     }
 }
--- tests/KafkaEnumerable.UnitTests/Tests/Thresholds.cs
+++ tests/KafkaEnumerable.UnitTests/Tests/Thresholds.cs
@@ -1,12 +1,158 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xunit;
 
-namespace KafkaEnumerable.UnitTests.Tests
+namespace KafkaEnumerable.UnitTests.Tests;
+
+public class ThresholdsTests
 {
-    internal class Thresholds
+    static readonly string Topic = nameof(ThresholdsTests);
+    static readonly TimeSpan TestTimeout = TimeSpan.FromSeconds(10);
+
+    [Fact]
+    public void Multi_Should_Respect_Thresholds_When_Configured()
     {
+        using var cts = new CancellationTokenSource(TestTimeout);
+        var consumers = Enumerable.Range(0, 3).Select(_ => new InMemoryConsumer<byte[], byte[]>(
+            Topic,
+            v => v,
+            new Dictionary<int, byte[][]>
+            {
+                [0] = Enumerable.Range(0, 100).Select(_ => Array.Empty<byte>()).ToArray()
+            })).ToArray();
+
+        var stream = KafkaEnumerable.Multiple(consumers, thresholds: new[] { 100, 100, 100 }, cancellationToken: cts.Token);
+
+        stream.Take(100).All(m => m.Consumer == consumers[0] && m.HasData).Should().BeTrue();
+        stream.Take(100).All(m => m.Consumer == consumers[1] && m.HasData).Should().BeTrue();
+        stream.Take(100).All(m => m.Consumer == consumers[2] && m.HasData).Should().BeTrue();
+
+        stream.Take(3).All(m => m.HasData && m.ConsumeResult!.IsPartitionEOF).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Priority_Should_Respect_Thresholds_When_Configured()
+    {
+        using var cts = new CancellationTokenSource(TestTimeout);
+        var consumers = Enumerable.Range(0, 3).Select(_ => new InMemoryConsumer<byte[], byte[]>(
+            Topic,
+            v => v,
+            new Dictionary<int, byte[][]>
+            {
+                [0] = Enumerable.Range(0, 100).Select(_ => Array.Empty<byte>()).ToArray()
+            })).ToArray();
+
+        var stream = KafkaEnumerable.Priority(consumers, thresholds: new[] { 100, 100, 100 }, cancellationToken: cts.Token);
+
+        stream.Take(100).All(m => m.Priority == 0 && m.HasData).Should().BeTrue();
+        stream.First().ConsumeResult!.IsPartitionEOF.Should().BeTrue();
+        stream.Take(100).All(m => m.Priority == 1 && m.HasData).Should().BeTrue();
+        stream.First().ConsumeResult!.IsPartitionEOF.Should().BeTrue();
+        stream.Take(100).All(m => m.Priority == 2 && m.HasData).Should().BeTrue();
+        stream.First().ConsumeResult!.IsPartitionEOF.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Multiple_Should_Move_Between_Priorities_Once_Threshold_Reached()
+    {
+        using var cts = new CancellationTokenSource(TestTimeout);
+        var consumers = Enumerable.Range(0, 3).Select(_ => new InMemoryConsumer<byte[], byte[]>(
+            Topic,
+            v => v,
+            new Dictionary<int, byte[][]>
+            {
+                [0] = Enumerable.Range(0, 100).Select(_ => Array.Empty<byte>()).ToArray()
+            })).ToArray();
+
+        var stream = KafkaEnumerable.Multiple(consumers, thresholds: new[] { 50, 50, 50 }, cancellationToken: cts.Token);
+
+        stream.Take(50).All(m => m.Consumer == consumers[0] && m.HasData).Should().BeTrue();
+        stream.Take(50).All(m => m.Consumer == consumers[1] && m.HasData).Should().BeTrue();
+        stream.Take(50).All(m => m.Consumer == consumers[2] && m.HasData).Should().BeTrue();
+        stream.Take(50).All(m => m.Consumer == consumers[0] && m.HasData).Should().BeTrue();
+        stream.Take(50).All(m => m.Consumer == consumers[1] && m.HasData).Should().BeTrue();
+        stream.Take(50).All(m => m.Consumer == consumers[2] && m.HasData).Should().BeTrue();
+        stream.Take(3).All(m => m.HasData && m.ConsumeResult!.IsPartitionEOF).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Multiple_Should_Update_Priority_When_Null_Is_Returned()
+    {
+        using var cts = new CancellationTokenSource(TestTimeout);
+
+        var messages = Enumerable.Range(0, 50).Select(_ => Array.Empty<byte>()).ToArray();
+        var @break = new[] { (byte[])null! };
+
+        var consumers = Enumerable.Range(0, 3).Select(_ => new InMemoryConsumer<byte[], byte[]>(
+            Topic,
+            v => v,
+            new Dictionary<int, byte[][]>
+            {
+                [0] = messages.Concat(@break).Concat(messages).Concat(messages).ToArray()
+            })).ToArray();
+
+        var stream = KafkaEnumerable.Multiple(consumers, thresholds: new[] { 100, 100, 100 }, cancellationToken: cts.Token);
+
+        stream.Take(50).All(m => m.Consumer == consumers[0] && m.HasData).Should().BeTrue();
+        stream.Take(50).All(m => m.Consumer == consumers[1] && m.HasData).Should().BeTrue();
+        stream.Take(50).All(m => m.Consumer == consumers[2] && m.HasData).Should().BeTrue();
+        stream.Take(100).All(m => m.Consumer == consumers[0] && m.HasData).Should().BeTrue();
+        stream.Take(100).All(m => m.Consumer == consumers[1] && m.HasData).Should().BeTrue();
+        stream.Take(100).All(m => m.Consumer == consumers[2] && m.HasData).Should().BeTrue();
+        stream.Take(3).All(m => m.HasData && m.ConsumeResult!.IsPartitionEOF).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Priority_Should_Still_Process_First_Priority_If_Threshold_Reached()
+    {
+        using var cts = new CancellationTokenSource(TestTimeout);
+        var consumers = Enumerable.Range(0, 3).Select(_ => new InMemoryConsumer<byte[], byte[]>(
+            Topic,
+            v => v,
+            new Dictionary<int, byte[][]>
+            {
+                [0] = Enumerable.Range(0, 100).Select(_ => Array.Empty<byte>()).ToArray()
+            })).ToArray();
+
+        var stream = KafkaEnumerable.Priority(consumers, thresholds: new[] { 50, 50, 50 }, cancellationToken: cts.Token);
+
+        stream.Take(100).All(m => m.Priority == 0 && m.HasData).Should().BeTrue();
+        stream.First().ConsumeResult!.IsPartitionEOF.Should().BeTrue();
+        stream.Take(100).All(m => m.Priority == 1 && m.HasData).Should().BeTrue();
+        stream.First().ConsumeResult!.IsPartitionEOF.Should().BeTrue();
+        stream.Take(100).All(m => m.Priority == 2 && m.HasData).Should().BeTrue();
+        stream.First().ConsumeResult!.IsPartitionEOF.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Priority_Should_Move_To_Next_Priority_If_Returned_Null()
+    {
+        var @break = new byte[][] { null, null };
+        var messages = Enumerable.Range(0, 50).Select(_ => Array.Empty<byte>()).ToArray();
+
+        using var cts = new CancellationTokenSource(TestTimeout);
+        var consumers = Enumerable.Range(0, 3).Select(_ => new InMemoryConsumer<byte[], byte[]>(
+            Topic,
+            v => v,
+            new Dictionary<int, byte[][]>
+            {
+                [0] = messages.Concat(@break).Concat(messages).ToArray()
+            })).ToArray();
+
+        var stream = KafkaEnumerable.Priority(consumers, thresholds: new[] { 100, 100, 100 }, cancellationToken: cts.Token);
+
+        stream.Take(50).All(m => m.Priority == 0 && m.HasData).Should().BeTrue();
+        stream.Take(50).All(m => m.Priority == 1 && m.HasData).Should().BeTrue();
+        stream.Take(50).All(m => m.Priority == 0 && m.HasData).Should().BeTrue();
+        stream.First().ConsumeResult!.IsPartitionEOF.Should().BeTrue();
+        stream.Take(50).All(m => m.Priority == 1 && m.HasData).Should().BeTrue();
+        stream.First().ConsumeResult!.IsPartitionEOF.Should().BeTrue();
+        stream.Take(100).All(m => m.Priority == 2 && m.HasData).Should().BeTrue();
+        stream.First().ConsumeResult!.IsPartitionEOF.Should().BeTrue();
     }
 }
